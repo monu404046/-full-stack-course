5bef8b5e9559a9a9e00fcae544fe79056f4af244
monu404046 <Administrator@PLLAB-25.ASIETDomain.com>
2019-07-25 12:35:02 +0530

    operator

---
+++ htmlcss/js/script.js
@@ -0,0 +1,47 @@
+function get()
+{
+    var n1=parseInt(document.getElementById("n1").value);
+    var n2=parseInt(document.getElementById("n2").value);
+    var oper=document.getElementById("oper").value;
+    var r;
+
+
+   if(oper=="add")
+   {
+      r=addn(n1,n2) 
+   }
+   else if(oper="sub")
+   {
+       r=subn(n1,n2)
+   }
+   else if(oper="mul")
+   {
+       r=muln(n1,n2)
+   }
+   else if(oper="div")
+   {
+       r=divn(n1,n2)
+   }
+  document.getElementById("r").innerHTML=n;
+}
+function addn(a,b)
+{
+    var c=a+b;
+    return c;
+
+}
+function subn(a,b)
+{
+    var c=a-b;
+    return c;
+}
+function muln(a,b)
+{
+    var c=a*b;
+    return c;
+}
+function div(a,b)
+{
+    var c=a/b;
+    return c;
+}
